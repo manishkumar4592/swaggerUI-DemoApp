362bc7ab46a7bccf94e900d0b6cb9ef158413546
Manish Kumar <manishkumar.gurjar@dotsquares.com>
2021-08-18 19:35:44 +0530

    Add project code

---
+++ WebAPI-SwaggerDemo/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebAPI_SwaggerDemo.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ WebAPI-SwaggerDemo/Controllers/ChatbotController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI_SwaggerDemo.Controllers
+{
+    public class ChatbotController : ApiController
+    {
+        // GET: api/Chatbot
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Chatbot/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Chatbot
+        public dynamic Post([FromBody]string value)
+        {
+            return new Employee
+            {
+                EmployeeId = 1,
+                FirstName = "Manish",
+                LastName = "Kumar",
+                City = "Jaipur",
+                ZipCoe = "302004"
+            };
+        }
+
+        // PUT: api/Chatbot/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Chatbot/5
+        public void Delete(int id)
+        {
+        }
+
+        #region ///   Classes   ///
+        public class Employee
+        {
+            public int EmployeeId { get; set; }
+
+            public string FirstName { get; set; }
+
+            public string LastName { get; set; }
+
+            public string City { get; set; }
+
+            public string ZipCoe { get; set; }
+
+        }
+        #endregion
+
+    }
+}
